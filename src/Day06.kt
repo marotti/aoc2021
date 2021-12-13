7e234d5f8dfb95c76a311a6189d2982aca0acff2
josh@marotti.com <Gtu8o7t6f1!>
2021-12-12 21:45:23 -0500

    Day six is done with the help of some maps

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,55 +1,48 @@
-val BABY_START_CYCLE = 8
-val ADULT_START_CYCLE = 6
+const val BABY_START_CYCLE = 8
+const val ADULT_START_CYCLE = 6
+const val SPAWN_TIME = 0
 
-data class LanternFish(var cycle: Int) {
-  fun cycleDay(): Any = if (cycle == 0) cycle = ADULT_START_CYCLE else cycle--
-  fun willSpawnBaby(): Boolean = cycle == 0
-}
-
-data class Ocean(val fishies: MutableList<LanternFish>) {
+data class Ocean(var fishies: MutableMap<Int, Long>) {
   fun oceanCycle() {
-    var newFishies = 0
-    fishies.asSequence().forEach { fish ->
-      if (fish.willSpawnBaby()) newFishies++
-      fish.cycleDay()
-    }
-    repeat(newFishies) { fishies.add(LanternFish(BABY_START_CYCLE)) }
+    val nextCycle = fishies.filter { it.key != 0 }.mapKeys { it.key - 1 }.toMutableMap()
+    nextCycle[ADULT_START_CYCLE] = (nextCycle[ADULT_START_CYCLE] ?: 0) + (fishies[SPAWN_TIME] ?: 0)
+    nextCycle[BABY_START_CYCLE] = fishies[SPAWN_TIME] ?: 0
+    fishies = nextCycle
   }
 
   companion object {
     fun loadInitialFishies(input: List<String>) =
+      // cycle -> num of fish in a cycle
       Ocean(
         input
           .first()
           .split(",")
           .map { it.toInt() }
-          .map { LanternFish(it) }
-          .toMutableList()
+          .groupingBy { it }
+          .eachCount()
+          .mapValues { it.value.toLong() }
+          .toMutableMap()
       )
   }
 }
 
 fun main() {
-  fun part1(input: List<String>): Int {
+  fun part1(input: List<String>): Long {
     val ocean = Ocean.loadInitialFishies(input)
     repeat(80) { ocean.oceanCycle() }
-    return ocean.fishies.size
+    return ocean.fishies.values.sum()
   }
 
   fun part2(input: List<String>): Long {
     val ocean = Ocean.loadInitialFishies(input)
-    var cycle = 1
-    repeat(256) {
-      println(cycle++)
-      ocean.oceanCycle()
-    }
-    return ocean.fishies.size.toLong()
+    repeat(256) { ocean.oceanCycle() }
+    return ocean.fishies.values.sum()
   }
 
   // test if implementation meets criteria from the description, like:
   val testInput = readInput("Day06_test")
   println(part1(testInput))
-  check(part1(testInput) == 5934)
+  check(part1(testInput) == 5934.toLong())
   println(part2(testInput))
   check(part2(testInput) == 26984457539)
 
